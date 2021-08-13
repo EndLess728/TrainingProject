ba75e0b986a8a0b18dcdbd2b8bad4a08ad60c4fc
Mantu <=_=>
2021-08-13 11:51:21 +0530

    Initial Commit

---
+++ Training/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Training
+//
+//  Created by IOS on 13/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
